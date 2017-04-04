a4958ef4455f580dc57ee37ca0018ff1f26abb09
sumit <sgupta40@sapient.com>
2017-04-04 10:57:31 +0530

    updated the files

---
+++ src/main/java/com/csr/CsrapiApplication.java
@@ -0,0 +1,12 @@
+package com.csr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsrapiApplication.class, args);
+	}
+}

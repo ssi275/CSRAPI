c48c2deffb6489e3a06bd2140a7dceb0873f39fd
sumit <sgupta40@sapient.com>
2017-04-06 14:21:24 +0530

    updated basic methods in the spring boot

--- src/main/java/com/csr/controller/UserController.java
+++ src/main/java/com/csr/controller/UserController.java
@@ -1,14 +1,51 @@
 package com.csr.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
 	@RequestMapping("/")
-	private String getUser() {
+	private String testmethod() {
+		log.info("test method hit by us");
 	        return "First Controller";
 
 	}
+	
+	
+	@RequestMapping(value = "/users", method = { RequestMethod.POST })
+	private boolean createUser()
+	{
+		log.info("create method hit by us");
+		return true;
+	}
+	
+	//TODO: Need to be changed by user pojo
+	@RequestMapping(value = "/users", method = { RequestMethod.GET })
+	private String getUser()
+	{
+		log.info("get method hit by us");
+		return "user";
+	}
+	
+	//TODO: Need to be changed by user pojo
+	@RequestMapping(value = "/users", method = { RequestMethod.PUT })
+	private String updateUser()
+	{
+		log.info("put method hit by us");
+		return "user";
+	}
+	
+	@RequestMapping(value = "/users", method = { RequestMethod.DELETE })
+	private boolean deleteUser()
+	{
+		log.info("delete method hit by us");
+		return false;
+	}
+
 }

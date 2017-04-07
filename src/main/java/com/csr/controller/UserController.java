c91b6ff3ac41fc59915b5ad1afad0d12b46edab9
unknown <ssi275@delssi27566277.sapient.com>
2017-04-07 17:29:05 +0530

    commiting user controller

--- src/main/java/com/csr/controller/UserController.java
+++ src/main/java/com/csr/controller/UserController.java
@@ -1,36 +1,51 @@
 package com.csr.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.csr.entity.User;
+import com.csr.mongoRepositories.UserRepository;
+
 @RestController
 public class UserController {
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	@Autowired
+	private UserRepository userRepository;
 	
-	@RequestMapping("/")
-	private String testmethod() {
-		log.info("test method hit by us");
-	        return "First Controller";
-
-	}
-	
+//	@RequestMapping("/")
+//	private String testmethod() {
+//		log.info("test method hit by us");
+//	        return "First Controller";
+//
+//	}
+//	
 	
 	@RequestMapping(value = "/users", method = { RequestMethod.POST })
-	private boolean createUser()
-	{
+	private boolean createUser(@RequestBody User user)
+	{	
+		userRepository.save(user);
 		log.info("create method hit by us");
 		return true;
 	}
 	
 	//TODO: Need to be changed by user pojo
 	@RequestMapping(value = "/users", method = { RequestMethod.GET })
-	private String getUser()
-	{
-		log.info("get method hit by us");
-		return "user";
+	private List<User> getUser()
+	{		
+			List<User> user= userRepository.findAll();
+//			user.get(0).getUserName();
+			System.out.println("" +"hello");
+			log.info("get method hit by us");
+			return  user;
+//		log.info("get method hit by us");
+//		return "user";
 	}
 	
 	//TODO: Need to be changed by user pojo

f88f3836ceeee597c3cff8a5da65bc0af3cf447c
Shruti Singh <ssi275@sapient.com>
2017-06-14 16:11:52 +0530

    Login/Register api

--- src/main/java/com/csr/controller/UserController.java
+++ src/main/java/com/csr/controller/UserController.java
@@ -18,33 +18,71 @@ public class UserController {
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@RequestMapping(value = "/users", method = { RequestMethod.POST })
-	public  boolean createUser(@RequestBody User user)
-	{	
-		if(user.getUserId()!= null){
+	public boolean createUser(@RequestBody User user) {
+		if (user.getUserId() != null) {
 			userRepository.save(user);
 			log.info("Inside createUser method of User controller");
-			return true;}
-		else
+			return true;
+		} else
 			return false;
 	}
-	
+
 	@RequestMapping(value = "/users", method = { RequestMethod.GET })
-	public List<User> getUser()
-	{		
-			List<User> user= userRepository.findAll();
-			System.out.println("system variable"+System.getenv("spring.data.mongodb.uri"));
-			log.info("Inside getUser method of User controller"+System.getenv("spring.data.mongodb.uri"));
-			return  user;
+	public List<User> getUser() {
+		List<User> user = userRepository.findAll();
+		System.out.println("system variable" + System.getenv("spring.data.mongodb.uri"));
+		log.info("Inside getUser method of User controller" + System.getenv("spring.data.mongodb.uri"));
+		return user;
 	}
-	
+
 	@RequestMapping(value = "/users", method = { RequestMethod.PUT })
-	public static String updateUser()
-	{
+	public static String updateUser() {
 		log.info("Inside updateUser method of User controller");
 		return "user";
 	}
-	
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String LoginUser(@RequestBody User user) {
+		String userId = user.getUserId();
+		String password = user.getPassword();
+		User U = userRepository.findByUserId(userId);
+
+		if (null != U && (U.getUserId().equals(userId)) && (U.getPassword().equals(password))) {
+			return "welcome";
+		} else if (null == U) {
+			return "You need to register";
+		} else {
+			return "Wrong Credentials";
+		}
+
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public void AddNewUser(@RequestBody User newUser) {
+
+		List<User> list = userRepository.findAll();
+		for (int i = 0; i < list.size(); i++) {
+			User u = list.get(i);
+			if (null != newUser.getUserId() && null != newUser.getPassword() && null != newUser.getUserName()
+					&& null != newUser.getUserType()) {
+				if (newUser.getUserId().equals(u.getUserId())) {
+					System.out.println("User Already exist");
+
+				} else {
+					User user = new User();
+					user.setUserId(newUser.getUserId());
+					user.setPassword(newUser.getPassword());
+					user.setUserName(newUser.getUserName());
+					user.setUserType(newUser.getUserType());
+					userRepository.save(user);
+
+				}
+			}
+
+		}
+
+	}
 
 }
--- src/main/java/com/csr/mongoRepositories/UserRepository.java
+++ src/main/java/com/csr/mongoRepositories/UserRepository.java
@@ -1,10 +1,15 @@
 package com.csr.mongoRepositories;
 
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.csr.entity.User;
 
 public interface UserRepository extends MongoRepository<User, String>{
+	public User findByUserId( String userId);
+	//public Charity findByNgoId(String ngoId);
+	public List<User> findAll();
 
 }

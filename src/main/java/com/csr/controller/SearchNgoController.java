1b9ca5e481f19b0991ad7639600b8eb6e1b642d7
spahw1 <spahwa2@sapient.com>
2017-11-21 15:56:17 +0530

    new endpoint for getting ngo by requirement

--- src/main/java/com/csr/controller/SearchNgoController.java
+++ src/main/java/com/csr/controller/SearchNgoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.csr.entity.Charity;
@@ -14,11 +15,19 @@ import com.csr.service.SearchService;
 @RestController
 public class SearchNgoController {
 
-	@Autowired
-	SearchService searchService;
+    @Autowired
+    SearchService searchService;
 
-	@RequestMapping(value = "/calculateDistance/{lat}/{lon}", method = { RequestMethod.GET })
-	public List<Charity> findNearbyNgos(@PathVariable Double lat, @PathVariable Double lon) {
-		return searchService.fetchNearbyNgoList(lat, lon);
-	}
+    @RequestMapping(value = "/calculateDistance/{lat}/{lon}", method = {RequestMethod.GET})
+    public List<Charity> findNearbyNgos(
+            @PathVariable Double lat, @PathVariable Double lon) {
+        return searchService.fetchNearbyNgoList(lat, lon);
+    }
+
+    @RequestMapping(value = "charity/requirement/{lat}/{lon}", method = {RequestMethod.GET})
+    public List<Charity> findNearbyNgosByRequirement(
+            @PathVariable Double lat, @PathVariable Double lon,
+            @RequestParam(value = "category", required = true) String category) {
+        return searchService.fetchNearbyNgoListForRequiredCategory(category, lat, lon);
+    }
 }
